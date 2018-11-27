37ebdbce7027a18646a277bf4f69ba4763910c66
seakhtar <sarah.akhtar@dakota-consulting.com>
2018-11-27 13:13:13 -0500

    Update awardees-static.js

--- assets/js/awardees-static.js
+++ assets/js/awardees-static.js
@@ -65,6 +65,8 @@ $(function() {
           $listContainer = $(list.listContainer);
           $listContainer.show();
           $listContainer.find('.results-company-title').text(company.values().awardeeName);
+          var listTitle = $listContainer.find('.results-company-title').text();
+          $listContainer.find('.results-company-title').text(listTitle.replace('&amp;', '&'));
           $listContainer.find('.results-company-title').show();
           $listContainer.find('.results-phase').show();
         }
